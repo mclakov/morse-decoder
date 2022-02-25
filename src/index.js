1e121c86ee1c00fd5a8299cb29c2ebe646db6f02
Maklakov Roman <maklakov@cate.ru>
2022-02-25 17:20:40 +0300

    feat: morse-fun

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    str2 = "";
+    let l= 0;
+    for (let i=0; i < expr.length/10; i++) {
+        let str = "";
+        for (let j=0; j < 10; j++) {
+            str = str + expr[l];
+            l = l + 1;
+        }
+        arr.push(str);
+    };
+    arr.map(elem => {
+        let letter = '';
+        for (let y = 0; y < 10; y = y + 2){
+            if ((elem[y] + elem[y+1] != "00") && (elem[y] + elem[y+1] == "10")) {
+                letter = letter + ".";
+            }
+            if ((elem[y] + elem[y+1] != "00") && (elem[y] + elem[y+1] == "11")) {
+                letter = letter + "-";
+            }
+            if ((elem[y] + elem[y+1] != "00") && (elem[y] + elem[y+1] == "**")) {
+                letter = letter + " ";
+                y = 10;
+            }
+        }
+        str2 = letter == " " ? str2 + " " : str2+MORSE_TABLE[letter];
+    });
+    return str2;
 }
 
 module.exports = {
